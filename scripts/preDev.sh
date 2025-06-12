ebe753cbae2339381037039297268fa26bd6e686
Shane O'Sullivan <shaneosullivan1@gmail.com>
2025-06-12 11:57:23 +0100

    DFix bug in preDev.sh script

--- scripts/preDev.sh
+++ scripts/preDev.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 
 mkdir -p "$PROJECT_DIR/public/_next/static/chunks"
-copy "$PROJECT_DIR/public/jq.wasm" "$PROJECT_DIR/public/_next/static/chunks/jq.wasm"
+cp "$PROJECT_DIR/public/jq.wasm" "$PROJECT_DIR/public/_next/static/chunks/jq.wasm"
