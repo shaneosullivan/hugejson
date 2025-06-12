ec14af8cd07a41e1c6326800a07b9af0d1c021d5
Shane O'Sullivan <shaneosullivan1@gmail.com>
2025-06-12 15:31:16 +0100

    Hackily patch the jq-web node module code

---
+++ scripts/patchJqWeb.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+const jqWebPath = path.join(__dirname, "..", "node_modules", "jq-web", "jq.js");
+
+console.log("jqWebPath", jqWebPath);
+
+const contents = fs.readFileSync(jqWebPath, "utf8");
+
+const lines = contents.split("\n");
+
+let found = false;
+const patchedLines = lines.map((line) => {
+  // Patch the line to use the correct path for jq.wasm
+  if (line.includes("require('fs')")) {
+    found = true;
+    return line.replace(
+      `require('fs')`,
+      `{
+  readFile: () => {},
+  writeFile: () => {},
+  existsSync: () => false,
+  readFileSync: () => '',
+  writeFileSync: () => {},
+  promises: {
+    readFile: () => Promise.resolve(''),
+    writeFile: () => Promise.resolve(),
+  }
+}`
+    );
+  }
+  return line;
+});
+
+if (found) {
+  fs.writeFileSync(jqWebPath, patchedLines.join("\n"), "utf8");
+}
--- scripts/preExport.sh
+++ scripts/preExport.sh
@@ -5,3 +5,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 
 rm -rf "$PROJECT_DIR/public/_next"
+
+node "$PROJECT_DIR/../scripts/patchJqWeb.js" 
